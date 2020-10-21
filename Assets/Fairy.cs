14f8d39ad466539b4d17114b6c270e655b2de412
XimenaCortez <54585679+XimenaCortez@users.noreply.github.com>
2020-10-21 11:50:15 -0500

    Initial Upload

---
+++ Assets/Fairy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fairy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        ProcessInput();
+       
+    }
+
+    private void ProcessInput(){
+        if (Input.GetKey(KeyCode.Space)){
+            print("Spaced Pressed");
+        }
+        else if (Input.GetKey(KeyCode.A)){
+            print("Rotating Left");
+        }
+        else if (Input.GetKey(KeyCode.D)){
+            print("Rotating Right");
+        }
+    }
+}
